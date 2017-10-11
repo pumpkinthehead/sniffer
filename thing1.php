bab814b0e91cdbaba05312c6f6ea316fb7724169
Moo <pumpkinthehead@hotmail.com>
2017-10-10 20:02:11 -0600

    Initial commit

---
+++ thing1.php
@@ -0,0 +1,11 @@
+<?php
+
+$thing = true;
+
+if ($thing == true) {
+	echo 'yo';
+} else {
+	echo 'no';
+}
+
+
---
+++ thing2.php
@@ -0,0 +1,11 @@
+<?php
+
+$thing = false;
+
+if (true == $thing) {
+	echo 'yo';
+} else {
+	echo 'no';
+}
+
+
